8d27a71fd8489fe0f72d9b0677fc43e8db6bf6f4
twerho3 <twerho3@gatech.edu>
2021-02-24 23:28:09 -0500

    add title page

---
+++ src/sample/FirstSreen.java
@@ -0,0 +1,43 @@
+package sample;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+public class FirstSreen {
+    String title = "Space Game!";
+    Button exit;
+    Button next;
+    Scene page1;
+
+    /**
+     * Initializes the first page. will initialize the same page every time, button setOnActions must be set up in main
+     * page
+     */
+    public FirstSreen() {
+        exit = new Button ("Exit Game");
+        next = new Button("New Game");
+        Label usedtitle = new Label(title);
+        usedtitle.setTextFill(Color.WHITE);
+        usedtitle.setFont(Font.font("Lao MN", FontWeight.BOLD, 40));
+        VBox layoutPage1 = new VBox(1);
+        layoutPage1.setStyle("-fx-background-image: url('sample/Background.png');");
+        layoutPage1.setSpacing(50);
+        layoutPage1.setAlignment(Pos.BASELINE_CENTER);
+        page1 = new Scene(layoutPage1,800,800);
+        layoutPage1.getChildren().addAll(usedtitle,next,exit);
+    }
+
+    /**
+     * Returns the first Scene
+     * @return the Scene which was initalized.
+     */
+    public Scene getScene() {
+        return page1;
+    }
+}
