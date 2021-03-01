e8ff10c8a03745a3c739f338b6372f78d1c42c7b
twerho3 <twerho3@gatech.edu>
2021-02-28 22:38:31 -0500

    add FirstScreenTest

---
+++ tests/FirstScreenTest.java
@@ -0,0 +1,22 @@
+import main.Controller;
+import javafx.stage.Stage;
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+public class FirstScreenTest extends ApplicationTest {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Controller controller = new Controller();
+        controller.start(primaryStage);
+    }
+
+    @Test
+    public void testTitle() {
+        verifyThat("Space Game!", NodeMatchers.isNotNull());
+    }
+    @Test
+    public void testNG() {
+        verifyThat("New Game", NodeMatchers.isNotNull());
+    }
+}
