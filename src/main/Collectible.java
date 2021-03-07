dd180fc7da29d4da6bd4e9b58e6594a3a1b420ea
atabbaa6 <atabbaa6@gatech.edu>
2021-03-06 23:25:37 -0500

    Beginnings of Room Class and its Controller. Not fully accurate/functional.

---
+++ src/main/Collectible.java
@@ -0,0 +1,15 @@
+package main;
+
+
+/**
+ * This interface should be implemented for all objects that implement Locatable but can also be carried by the Player.
+ */
+public interface Collectible {
+
+    /**
+     * This method is used to retrieve the location of an object in the room that can be carried
+     * @return a List of length two with the x and y coordinates of the location.
+     * x and y coordinates begin at 0 in top left corner
+     */
+    int[] getPosition();
+}
